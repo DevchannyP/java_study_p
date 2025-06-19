f49cf853330d07d517cc306ea9b3d1ef84f57b43
DevchannyP <codingobsession09@gmail.com>
2025-06-19 23:28:21 +0900

    Update Exam2.java

--- chap12/src/ex2_set/Exam2.java
+++ chap12/src/ex2_set/Exam2.java
@@ -4,13 +4,7 @@ import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
-/*
- * Lotto 번호 생성하기
- * 1. 1 ~ 45사이의 임의의 값 중복되지 않는 6개 선택.
- * 2. 정렬하여 출력하기
- * 
- * TreeSet을 이용하기  
- */
+
 public class Exam2 {
 	public static void main(String[] args) {
 		Set<Integer> lotto = new TreeSet<>();
